82302911f6dc1fb670cb673f67ce3740bcf46c0a
AfroMonkey <moisalejandro@gmail.com>
2016-10-13 18:15:54 -0500

    [IMP] group by contract_type

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -4,4 +4,4 @@ from openerp import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    contract_type = fields.Selection(related="order_id.contract_type", readonly=True)
+    contract_type = fields.Selection(related="order_id.contract_type", store=True, readonly=True)
